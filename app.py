6d120220d4b49c0d273bce22a5bfe1b131bc8a21
Simurgotrizz <136985568+Simurgotrizz@users.noreply.github.com>
2023-07-15 11:44:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import requests
 import streamlit as st
 from streamlit_lottie import st_lottie 
-import pyttsx3
+import pyttsx3 
 import datetime
 import speech_recognition as sr
 import webbrowser
